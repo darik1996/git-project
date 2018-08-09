92188efdb93e23fc84a07809f0e04275e72879df
root <root@localhost.localdomain>
2018-08-09 16:03:11 +0300

    second commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle('Главная');
 ?> 
-
+<h2>Changes for second commit</h2>
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
